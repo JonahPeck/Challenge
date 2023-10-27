eb87941bc5a9d12fe329d66baf5152497206940f
JonahPeck <PeckJonahC@gmail.com>
2023-10-27 19:48:58 -0400

    backend

--- server/app.py
+++ server/app.py
@@ -3,10 +3,9 @@ from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from models import db, User
 
-from models import db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usertickets.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -17,10 +16,76 @@ api = Api(app)
 
 class AllTickets(Resource):
     def get(self):
-        all_tickets = User.query.all()
-        dict_tickets = []
-        for ticket in all_tickets:
-            dict_tickets.append(ticket.to_dict())
-        return make_response(dict_tickets, 200)
-api.add_resource(AllTickets,'/home')
+        response_dict_list = [n.to_dict() for n in User.query.all()]
+        response = make_response(
+            response_dict_list,
+            200,
+        )
+        return response
+    def post(self):
+        new_ticket = User(
+            name = request.form['name'],
+            email = request.form['email'],
+            description = request.form['description'],
+            status = request.form['status']
+        )
+        db.session.add(new_ticket)
+        db.session.commit()
+
+        response_dict = new_ticket.to_dict()
+        response = make_response(
+            response_dict,
+            201,
+        )
+        return response
+    
+api.add_resource(AllTickets,'/tickets')
+
+class TicketById(Resource):
+    def get(self, id):
+        response_dict = User.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+        return response
+
+    def patch(self, id):
+        record = User.query.filter(User.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+            
+            db.session.add(record)
+            db.session.commit()
+
+            response_dict = record.to_dict()
+            response = make_response(
+                response_dict,
+                200
+            )
+
+            return response
+
+        def delete (self, id):
+
+            delete_ticket = User.query.filter_by(id=id).first()
+            if delete_ticket:
+                all_delete_ticket = User.query.filter_by(id=id).all()
+                db.session.delete(delete_ticket)
+                for delete_ticket in all_delete_ticket:
+                    db.session.delete(delete_ticket)
+                db.session.commit()
+                return make_response({}, 200)
+            else:
+                return make_response({"error":"Ticket not found"}, 400)
+
+api.add_resource(TicketById, '/tickets/<int:id>')
+
+if __name__ == '__main__':
+    app.run(port=5555)
+
+
+
+
 
--- server/models.py
+++ server/models.py
@@ -17,15 +17,30 @@ class User(db.Model, SerializerMixin):
     ticket_created = db.Column(db.DateTime, server_default=db.func.now())
 
     #validations for names and email
-    @validates('name')
+    @validates('email')
     def check_email(self, key, email):
         if "@" in email:
             return email 
         else:
-            raise Exception("Not a Valid Email, Must contain '@' symbol")
+            raise Exception("Not a Valid Email, must contain '@' symbol")
+    
+    @validates('name')
+    def check_name(self, key, name):
+        if not name:
+            raise ValueError("Name must be provided.")
+        return name
+
+    @validates('description')
+    def check_description(self, key, description):
+        if not description:
+            raise ValueError("Description must be provided.")
+        return description
+        
+
+        
 
 
 
-    def __repr__ (self):
-        return f'<User {self.name}, created a ticket at {ticket_created}>'
+    # def __repr__ (self):
+    #     return f'<User {self.name}, created a ticket at {ticket_created}>'
     
